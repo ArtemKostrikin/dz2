13faedcc449346397a159eb140795c0c992f6d8d
ArtemKostrikin <96254980+ArtemKostrikin@users.noreply.github.com>
2023-04-20 22:28:56 +0300

    Update dz2.sql

--- dz2.sql
+++ dz2.sql
@@ -37,13 +37,12 @@ VALUES
 ('e02', 22.18, 'OPEN'),
 ('e04', 9.50, 'CANCELLED');
 -- Создаем кейс для показа отдельной колонки 'full_order_status' с интересующими нас условиями отбора и заменой маски значений:
-SELECT 
-	employee_id AS 'Номер заказа',
-	amount AS 'Сумма заказа',
+SELECT id, employee_id, amount, order_status,
     CASE order_status 
 		WHEN 'OPEN' THEN 'Order is in open state'
 		WHEN 'CLOSED' THEN 'Order is closed'
 		WHEN 'CANCELLED' THEN 'Order is cancelled'
 	END AS 'full_order_status'	
 FROM orders;
+
 
